2bab18703b52df5751e09931d7a0c4d51eabd5c6
IrBK <irbk@IrBKs-MacBook-Air.local>
2024-04-10 23:34:35 -0700

    handle the API data locally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,20 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import "./styles.scss";
 import serviceTaxCalculator from "@/service/serviceTaxCalculator";
+import { useState } from 'react';
 
 export default function Home() {
 
+  let [income, setIncome] = useState();
+  let [year, setYear] = useState();
+
+
   const handleSubmit = (e:any) => {
     e.preventDefault();
-    serviceTaxCalculator();
+    
+    serviceTaxCalculator(income, year);
+    // setIncome();
+    // setYear();
   }
 
   return (
@@ -18,8 +26,8 @@ export default function Home() {
       <div className={styles.description}>
         
         <form onSubmit={handleSubmit}>
-          <input type="number" min="1" step="1" placeholder="Annual Income..." required />
-          <input type="number" min="1" step="1" placeholder="Tax Year..." required />
+          <input type="number" value={income} min="1" step="1" placeholder="Annual Income..." required onChange={(e:any) => setIncome(e.target.value)} />
+          <input type="number" value={year} min="1" step="1" placeholder="Tax Year..." required onChange={(e:any) => setYear(e.target.value)} />
           <div className="submit">
             <button type="submit">Send</button>
           </div>
--- src/service/serviceTaxCalculator.tsx
+++ src/service/serviceTaxCalculator.tsx
@@ -1,20 +1,40 @@
 import axios from "axios"
+import Home from "../app/page";
+
 
 const APIURL =  "http://localhost:5001/tax-calculator/"
-const serviceTaxCalculator = async () => {
-    let response 
-    //axios.get(APIURL).then( res => res).catch( error => console.log(error));
-    
+
+const serviceTaxCalculator = async (income: any, year: any) => {
+    let response
+
     try {
         response = await axios.get(APIURL);
-        var res = response.data
+        var res = response.data.tax_brackets;
+
         console.log("API response", res)
+
+        var amountTaxable, taxPayable, total = 0;
+        for(let i=0; i<res.length; i++){
+            if(res[i].max){
+                amountTaxable = res[i].max - res[i].min
+                taxPayable = amountTaxable * res[i].rate;
+            } else {
+               amountTaxable = income - res[i].min;
+               taxPayable = amountTaxable * res[i].rate;
+            }
+            total += taxPayable;
+           // console.log(amountTaxable);
+        }
 
     }catch(exception){
         console.log("exception", exception)
     }
     
-    return response
+    return (
+        <div>
+
+        </div>
+    )
 
 }
 
